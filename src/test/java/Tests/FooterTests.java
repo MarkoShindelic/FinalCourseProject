e869a4619edc84c9ac0a51a3a35714221763d6e5
Marko <marko.shindelic88@gmail.com>
2023-10-18 23:25:18 +0200

    redoing footer class

--- src/test/java/Tests/FooterTests.java
+++ src/test/java/Tests/FooterTests.java
@@ -7,50 +7,57 @@ import org.testng.annotations.Test;
 
 import java.util.ArrayList;
 
-public class Footer extends BaseTest {
 
-    @BeforeMethod
-    public void pageSetUp() {
-        driver.navigate().to("https://www.saucedemo.com/");
-        String validUsername = "standard_user";
-        String validPassword = "secret_sauce";
+public class FooterTests extends BaseTest{
 
 
-        loginPage.inputUsername(validUsername);
-        loginPage.inputPassword(validPassword);
-        loginPage.clickLoginButton();
+
+
+        @BeforeMethod
+        public void pageSetUp() {
+            driver.navigate().to("https://www.saucedemo.com/");
+            String validUsername = "standard_user";
+            String validPassword = "secret_sauce";
+
+
+            loginPage.inputUsername(validUsername);
+            loginPage.inputPassword(validPassword);
+            loginPage.clickLoginButton();
+        }
+
+        @Test
+        public void userCanVisitAllLinkedPages(){
+
+            footerPage.clickOnTwitterButton();
+            footerPage.clickOnFacebookButton();
+            footerPage.clickOnLinkedInButton();
+
+            ArrayList<String> tabList = new ArrayList<>(driver.getWindowHandles());
+
+            driver.switchTo().window(tabList.get(3));
+
+            String expectedURL = "https://twitter.com/saucelabs";
+            String actualURL = driver.getCurrentUrl();
+
+            Assert.assertEquals(actualURL, expectedURL);
+
+            driver.switchTo().window(tabList.get(2));
+
+            expectedURL = "https://www.facebook.com/saucelabs";
+            actualURL = driver.getCurrentUrl();
+
+            Assert.assertEquals(actualURL, expectedURL);
+
+            driver.switchTo().window(tabList.get(1));
+
+            expectedURL = "https://www.linkedin.com/company/sauce-labs/";
+            actualURL = driver.getCurrentUrl();
+
+            Assert.assertEquals(actualURL, expectedURL);
+
+        }
+
     }
 
-     @Test
-    public void userCanVisitAllLinkedPages(){
 
-        footerPage.clickOnTwitterButton();
-        footerPage.clickOnFacebookButton();
-        footerPage.clickOnLinkedInButton();
 
-        ArrayList<String> tabList = new ArrayList<>(driver.getWindowHandles());
-
-        driver.switchTo().window(tabList.get(3));
-
-        String expectedURL = "https://twitter.com/saucelabs";
-        String actualURL = driver.getCurrentUrl();
-
-        Assert.assertEquals(actualURL, expectedURL);
-
-        driver.switchTo().window(tabList.get(2));
-
-        expectedURL = "https://www.facebook.com/saucelabs";
-        actualURL = driver.getCurrentUrl();
-
-        Assert.assertEquals(actualURL, expectedURL);
-
-        driver.switchTo().window(tabList.get(1));
-
-        expectedURL = "https://www.linkedin.com/company/sauce-labs/";
-        actualURL = driver.getCurrentUrl();
-
-        Assert.assertEquals(actualURL, expectedURL);
-
-    }
-
-}
